ef62fca86385965f56bc281d321ba7c55b7f02f4
yydongsq <yydongsq@126.com>
2022-03-15 17:42:53 +0800

    2022-03-15 17:42 增加数据集管理子菜单，配置HanLP标准分词API。

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/demo/test/controller/JeecgDemoController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/demo/test/controller/JeecgDemoController.java
@@ -1,10 +1,24 @@
 package org.jeecg.modules.demo.test.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.jeecg.common.api.vo.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Description: TODO
@@ -17,9 +31,60 @@ import org.springframework.web.bind.annotation.RestController;
 public class JeecgDemoController {
     @GetMapping(value = "/hello")
     public Result<String> hello() {
+        //请求头中的token
+        String token= "c28328e81f7543bfb65c7830ab9089d11646989916771token";
+        //申请的接口地址
+        String url="http://comdo.hanlp.com/hanlp/v1/segment/standard";
+        //所有参数
+        String text="HanLP是GitHub上最成功的NLP项目，全球数百万NLP开发者的共同选择！";
+        Map<String,Object> params=new HashMap<String,Object>();
+        params.put("text", text);
+        //执行api
+        String result_HanLP = doHanlpApi(token,url,params);
+
         Result<String> result = new Result<String>();
-        result.setResult("I LOVE CYH!");
+        result.setResult(result_HanLP);
         result.setSuccess(true);
         return result;
+    }
+
+    public static String doHanlpApi(String token,String url,Map<String,Object> params) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            //添加header请求头，token请放在header里
+            httpPost.setHeader("token", token);
+            // 创建参数列表
+            List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+            if (params != null) {
+                for (String key : params.keySet()) {
+                    //所有参数依次放在paramList中
+                    paramList.add(new BasicNameValuePair(key, (String) params.get(key)));
+                }
+                //模拟表单
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList, "utf-8");
+                httpPost.setEntity(entity);
+            }
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+            return resultString;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(response!=null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
     }
 }
