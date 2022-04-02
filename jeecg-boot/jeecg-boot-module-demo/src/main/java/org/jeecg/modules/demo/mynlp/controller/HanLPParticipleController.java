10d21446087420b37c635e62eded627a09bbe6e4
yydongsq <yydongsq@126.com>
2022-04-02 14:01:39 +0800

    2022-04-02 14:01 协同测试提交

--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/mynlp/controller/HanLPParticipleController.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/mynlp/controller/HanLPParticipleController.java
@@ -23,7 +23,7 @@ public class HanLPParticipleController {
     @GetMapping(value = "/hello")
     public Result<String> hello(@RequestParam("type") String type) {
         ParticipleCommon participle = new ParticipleCommon();
-        String text="中国和美国都有自己的发展目标。中国坚持改革开放，科学发展，力争成为一个富裕强大文明的国家，造福于中国人民和世界人民，同时坚持独立自主外交，维护世界和平。";
+        String text="中国和美国都有自己的发展目标111111。中国坚持改革开放，科学发展，力争成为一个富裕强大文明的国家，造福于中国人民和世界人民，同时坚持独立自主外交，维护世界和平。";
         System.out.println(type);
         String hanLPParticiple = participle.getHanLPParticiple(text,type);
         Result<String> result = new Result<String>();
