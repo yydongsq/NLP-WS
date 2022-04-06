57b90528c8668064f346ab6d1b1c9f0062a21250
yydongsq <yydongsq@126.com>
2022-04-06 17:23:07 +0800

    2022-04-06 17:22 增加jieba分词展示功能

--- jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/mynlp/controller/JiebaParticipleController.java
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/mynlp/controller/JiebaParticipleController.java
@@ -1,11 +1,11 @@
 package org.jeecg.modules.demo.mynlp.controller;/**
  * @author sq
- * @create 2022-04-05-17:43
+ * @create 2022-04-06-16:14
  */
 
 import lombok.extern.slf4j.Slf4j;
 import org.jeecg.common.api.vo.Result;
-import org.jeecg.modules.demo.mynlp.common.HanLPParticipleCommon;
+import org.jeecg.modules.demo.mynlp.common.JiebaParticipleCommon;
 import org.jeecg.modules.demo.mynlp.common.ThulacParticipleCommon;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @Description: TODO
  * @author: ydy
- * @date: 2022年04月05日 17:43
+ * @date: 2022年04月06日 16:14
  */
 @RestController
-@RequestMapping("/mynlp/thulac")
+@RequestMapping("/mynlp/jieba")
 @Slf4j
-public class ThulacParticipleController {
-    @GetMapping(value = "/thulacParticiple")
+public class JiebaParticipleController {
+    @GetMapping(value = "/jiebaParticiple")
     public Result<String> hello(@RequestParam("type") String type) {
-        ThulacParticipleCommon participle = new ThulacParticipleCommon();
+        JiebaParticipleCommon participle = new JiebaParticipleCommon();
         String text="我也想过过过儿过过的生活";
         System.out.println(type);
         String thulacParticiple = participle.getThulacParticiple(text,type);
---
+++ jeecg-boot/jeecg-boot-module-demo/src/main/java/org/jeecg/modules/demo/mynlp/entity/JiebaParticipleData.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.demo.mynlp.entity;/**
+ * @author sq
+ * @create 2022-04-06-16:17
+ */
+
+import lombok.Data;
+
+/**
+ * @Description: TODO
+ * @author: ydy
+ * @date: 2022年04月06日 16:17
+ */
+@Data
+public class JiebaParticipleData {
+    private String code;
+    private JiebaParticipleModel[] data;
+}
